ee841a999aa784827be7f928b6acf6c6c76286d7
Aleksandar Adamovic <aadamovic@tehnik-design.com>
2015-05-01 18:52:03 -0400

    feat(sample): add webapi project

---
+++ sample/SampleWebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace SampleWebApp.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
