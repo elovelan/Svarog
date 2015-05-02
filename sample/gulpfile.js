fbddcc18bf1047f2242915357e5a5f2e3d8f3c85
Aleksandar Adamovic <aadamovic@tehnik-design.com>
2015-05-01 21:47:35 -0400

    feat: create web staging task

--- sample/gulpfile.js
+++ sample/gulpfile.js
@@ -4,9 +4,18 @@ var svarog = require('svarog');
 var buildConfig = {
   stdout: true,
   toolsVersion: 12,
-  solutionPath: './SampleWebApp.sln'
+  solutionPath: './SampleWebApp.sln',
+  staging: {
+    webProjectPath: './SampleWebApp/SampleWebApp.csproj',
+    properties: {
+      dir: '../+staging',
+      encryptionKey: 'Test123'
+    }
+  }
 };
 
 svarog(gulp, buildConfig);
 
 gulp.task('build', ['buildSolution'], function () {});
+
+gulp.task('stage', ['stageWebArtifacts'], function () {});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var msbuild = require("gulp-msbuild");
 var plug = require('gulp-load-plugins')();
+var object = require('lodash/object');
 
 var log = plug.util.log;
 
@@ -12,4 +13,33 @@ module.exports = function (gulp, config) {
     return gulp.src(solutionPath)
       .pipe(msbuild(config));
   });
+
+  gulp.task('stageWebArtifacts', function () {
+
+    var stagingConfig = getStagingConfig(config);
+
+    return gulp.src(stagingConfig.webProjectPath)
+      .pipe(msbuild(stagingConfig))
+  });
+
+  function getStagingConfig(config) {
+    var defaultStagingConfig = {
+      stdout: true,
+      toolsVersion: 12,
+      targets: ['Package'],
+      properties: {
+        outdir: '../+staging',
+        DeployEncryptKey: 'test1',
+        IncludeAppPool: true,
+        IncludeAppPool: true,
+        IncludeIisSettings: true,
+        DestinationIisVersion: 8,
+        UseMsdeployExe: true
+      }
+    };
+
+    //Override default config with the config defined in inheriting gulp file
+    //Very hacky - but good enough to get it going
+    return object.merge(defaultStagingConfig, config, config.staging);
+  }
 };
